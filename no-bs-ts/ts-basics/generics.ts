ca450149b6146a1e66b73f9f134e8379e16ced3a
Bryon Smith <bryon@smithauto.us>
2022-01-10 17:55:38 -0600

    minor change

--- no-bs-ts/ts-basics/generics.ts
+++ no-bs-ts/ts-basics/generics.ts
@@ -6,10 +6,24 @@ function customSetState<T>(initial: T): [() => void, (v: T) => void] {
 const [num, numSet] = customSetState<number>(3)
 const [first, firstSet] = customSetState<string>('bob')
 
-console.log(num())
-console.log(numSet(200))
-console.log(num())
+// console.log(num())
+// numSet(200)
+// console.log(num())
 
-console.log(first())
-console.log(firstSet('nancy'))
-console.log(first())
+// console.log(first())
+// firstSet('nancy')
+// console.log(first())
+
+// test  forEach from reduce with generics
+
+const nums = [1, 2, 3]
+const strings = ['a', 'b', 'c']
+
+function customForEach<T>(arr: T[], forEachFunc: (v: T) => void): void {
+  arr.reduce((a, v) => {
+    forEachFunc(v)
+    return undefined
+  }, undefined)
+}
+
+customForEach(strings, (v) => console.log(v))
