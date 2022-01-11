64c86477628afc58f020b93354a841b52d064251
Bryon Smith <bryon@smithauto.us>
2022-01-11 14:13:26 -0600

    minor change

--- no-bs-ts/ts-basics/generics.ts
+++ no-bs-ts/ts-basics/generics.ts
@@ -4,6 +4,7 @@ function customSetState<T>(initial: T): [() => void, (v: T) => void] {
 }
 
 const [num, numSet] = customSetState<number>(3)
+
 const [first, firstSet] = customSetState<string>('bob')
 
 // console.log(num())
@@ -16,14 +17,35 @@ const [first, firstSet] = customSetState<string>('bob')
 
 // test  forEach from reduce with generics
 
-const nums = [1, 2, 3]
-const strings = ['a', 'b', 'c']
+const nums = [1, 2, 3, 2, 3, 5, 6, 7, 8]
+const numString = ['1', '2', '3', '2', '3', '5', '6', '7', '8']
+const strings = ['a', 'b', 'c', 'b']
 
+// forEach
 function customForEach<T>(arr: T[], forEachFunc: (v: T) => void): void {
   arr.reduce((a, v) => {
     forEachFunc(v)
     return undefined
   }, undefined)
 }
+// customForEach(strings, (v) => console.log(v))
 
-customForEach(strings, (v) => console.log(v))
+// Filter
+function myFilter<T>(arr: T[], filterFunc: (v: T) => boolean): T[] {
+  return arr.reduce(
+    (acc: T[], cur: T) => (filterFunc(cur) ? [...acc, cur] : acc),
+    []
+  )
+}
+// console.log(myFilter<string>(strings, (v) => v === 'b'))
+// console.log(myFilter<number>(nums, (v) => v === 2))
+// console.log(myFilter<number>(nums, (v) => v % 2 === 0))
+
+// map
+function myMap<T, K>(arr: T[], mapFunc: (v: T) => K): K[] {
+  return arr.reduce((acc: K[], cur: T) => [...acc, mapFunc(cur)], [])
+}
+
+console.log(myMap(nums, (v) => v + 10))
+console.log(myMap(nums, (v) => v.toString()))
+console.log(myMap(numString, (v) => +v))
