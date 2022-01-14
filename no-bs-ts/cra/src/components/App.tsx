f7f6e5c0cdfe2cf52a513490e4b8833cc3d76358
Bryon Smith <bryon@smithauto.us>
2022-01-14 07:31:09 -0600

    minor change

--- no-bs-ts/cra/src/components/App.tsx
+++ no-bs-ts/cra/src/components/App.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useCallback, useState } from 'react'
+import React, { FC, useCallback, useState, useEffect } from 'react'
+import User from 'data.json'
 
 const Heading: FC<{ title: string }> = ({ title }) => <h2>{title}</h2>
 const Box: React.FunctionComponent = ({ children }) => <div>{children}</div>
@@ -16,12 +17,20 @@ const List: FC<{
 )
 
 interface Payload {
-  text: string
+  name: string
 }
 
 const items = ['hello', 'goodby', 'see ya!']
 function App() {
-  const [payload, setPayload] = useState<Payload | null>(null)
+  const [payload, setPayload] = useState<Payload[] | null>(null)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users?id=5')
+      .then((res) => res.json())
+      .then((data: Payload[]) => {
+        setPayload(data)
+      })
+  }, [])
 
   const listAlert = useCallback((item: string): void => {
     alert(item)
@@ -32,6 +41,7 @@ function App() {
       <Heading title="Hello Bryon" />
       <Box>Hello there</Box>
       <List items={items} onClick={listAlert} />
+      <Box>{JSON.stringify(payload?.[0])}</Box>
     </div>
   )
 }
