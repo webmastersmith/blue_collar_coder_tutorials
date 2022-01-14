3ea52f0b9e6c885fda61b3d546d0d6529a7938d9
Bryon Smith <bryon@smithauto.us>
2022-01-14 09:47:36 -0600

    minor change

--- no-bs-ts/cra/src/components/App.tsx
+++ no-bs-ts/cra/src/components/App.tsx
@@ -1,4 +1,11 @@
-import React, { FC, useCallback, useState, useEffect } from 'react'
+import React, {
+  FC,
+  useCallback,
+  useState,
+  useEffect,
+  useReducer,
+  useRef,
+} from 'react'
 import User from 'data.json'
 
 const Heading: FC<{ title: string }> = ({ title }) => <h2>{title}</h2>
@@ -25,23 +32,85 @@ function App() {
   const [payload, setPayload] = useState<Payload[] | null>(null)
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users?id=5')
-      .then((res) => res.json())
-      .then((data: Payload[]) => {
-        setPayload(data)
-      })
+    // fetch('https://jsonplaceholder.typicode.com/users?id=5')
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setPayload(data)
+    //   })
   }, [])
+
+  type ActionType =
+    | { type: 'ADD'; text: string }
+    | { type: 'REMOVE'; id: number }
+
+  interface Todo {
+    text: string
+    done: boolean
+    id: number
+  }
+  const reducer = (state: Todo[], action: ActionType) => {
+    switch (action.type) {
+      case 'ADD':
+        return [
+          ...state,
+          {
+            id: state.length,
+            text: action.text,
+            done: false,
+          },
+        ]
+      case 'REMOVE':
+        return state.filter((todo: Todo) => todo.id !== action.id)
+      default:
+        return state
+    }
+  }
+
+  const newTodoRef = useRef<HTMLInputElement>(null)
+
+  const [todos, dispatch] = useReducer(reducer, [])
 
   const listAlert = useCallback((item: string): void => {
     alert(item)
   }, [])
 
+  const addTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: 'ADD',
+        text: newTodoRef.current.value,
+      })
+      newTodoRef.current.value = ''
+    }
+  }, [])
   return (
     <div>
       <Heading title="Hello Bryon" />
       <Box>Hello there</Box>
       <List items={items} onClick={listAlert} />
-      <Box>{JSON.stringify(payload?.[0])}</Box>
+
+      <Heading title="Todos" />
+      {todos.map((todo: Todo) => {
+        return (
+          <div key={todo?.id}>
+            {todo?.text}
+            <button
+              onClick={() =>
+                dispatch({
+                  type: 'REMOVE',
+                  id: todo.id,
+                })
+              }
+            >
+              Remove
+            </button>
+          </div>
+        )
+      })}
+      <div>
+        <input type="text" ref={newTodoRef} />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
     </div>
   )
 }
